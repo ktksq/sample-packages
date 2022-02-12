5d74859b031af64aee586d615577862fb709d83e
ktksq <joker.like-that@hotmail.co.jp>
2022-02-12 17:19:29 +0900

    remove main

--- count_nulls/count_nulls.py
+++ count_nulls/count_nulls.py
@@ -3,6 +3,3 @@ from pyspark.sql import functions as f
 def nulls(sdf, column_name):
     num_nulls = sdf.where(f.col(column_name).isNull()).count()
     return num_nulls
-
-if __name__ == '__main__' :
-    nulls()

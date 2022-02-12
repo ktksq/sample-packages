739ceed2d4c7ed6092aebc0b0ff91b8f0613dd47
ktksq <joker.like-that@hotmail.co.jp>
2022-02-12 17:28:44 +0900

    rename

--- count_nulls/count_nulls.py
+++ count_nulls/count_nulls.py
@@ -1,5 +1,5 @@
 
 from pyspark.sql import functions as f
-def nulls(sdf, column_name):
+def cnt_nulls(sdf, column_name):
     num_nulls = sdf.where(f.col(column_name).isNull()).count()
     return num_nulls
--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
-from glob import glob
-from os.path import basename
-from os.path import splitext
-
 from setuptools import setup
 from setuptools import find_packages
-
-
-def _requires_from_file(filename):
-    return open(filename).read().splitlines()
-
 
 setup(
     name="count_nulls",
@@ -17,12 +8,4 @@ setup(
     description="サンプルパッケージ",
     author="ktksq",
     packages=find_packages()
-    # url="GitHubなどURL",
-    # package_dir={"": "src"},
-    # py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-    # include_package_data=True,
-    # zip_safe=False,
-    # install_requires=_requires_from_file('requirements.txt'),
-    # setup_requires=["pytest-runner"],
-    # tests_require=["pytest", "pytest-cov"]
 )
